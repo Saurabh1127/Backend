2ee9f2f7110dc066315db7cdba3c6d1e9aa3d7f8
Saurabh <saurabh11224477@gmail.com>
2025-08-21 22:21:10 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+   path:"./env"
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // ; (async () => {
+   //    try {
+   //       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME} `)
+   //       app.on("error", (error) => {
+   //          console.log("ERROR :", error);
+   //          throw error;
+   //       })
+   //       app.listen(process.env.PORT, () => {
+   //          console.log(`Server is running on PORT ${process.env.PORT}`)
+   //       })
+   //    }
+   //    catch (error) {
+   //       console.error("ERROR :", error)
+   //    }
+   // })()
